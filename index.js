dbeff4316abd63618e4409a9fb375efd2c80e07f
Jordan Jaffe <jordan.m.jaffe@gmail.com>
2018-01-13 05:27:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var height = "74"
 if (name ==="susan"){
   console.log("The name is correct")
 }
+else (name ==="susan"){
+  console.log("Expected the name to be Susan")
 
 // Don't worry about this
 module.exports = { name, height
